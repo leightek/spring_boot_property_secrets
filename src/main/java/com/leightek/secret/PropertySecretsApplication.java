52f6451323858c00ad39e34dd3fdfaa375ef2787
leightek <leighsuconsulting@gmail.com>
2022-12-23 00:28:10 -0800

    [PropertySecrets] Initialized repository

---
+++ src/main/java/com/leightek/secret/PropertySecretsApplication.java
@@ -0,0 +1,13 @@
+package com.leightek.secret;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertySecretsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertySecretsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leightek/secret/PropertySecretsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leightek.secret;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertySecretsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
